e77d6ebb3f52213ee2919e79e2dbbc63f3a0ff0f
aurelien ayet <Ayet>
2017-10-20 20:28:19 +0200

    updated the io.controller.js, now register the pres_id

--- tp/app/controllers/io.controller.js
+++ tp/app/controllers/io.controller.js
@@ -17,6 +17,8 @@ function IoController(){
 
 IoController.listen= function(httpServer){
         var socketMap= new Map();
+        var currentPresId;
+        var cptSlide;
         var io = require('socket.io')(httpServer);
         io.on('connection',function(socket){
         
@@ -34,18 +36,60 @@ IoController.listen= function(httpServer){
             {
                 if(json.CMD!=undefined && json.CMD!="PAUSE")
                 {
-                    ContentModel.read(json.PRES_ID, function (err, content) {
-                        if(!!err)
-                            {
-                                console.error(err);
-                                 return err;
-                            }
-                    content.src = "/contents/" + content.id;
-                    for (var i in socketMap){
-                        socketMap[i].emit('currentSlidEvent',content);
+                    if(json.CMD=="START")
+                    {
+                        currentPresId=json.PRES_ID;
+                        cptSlide=0;
                     }
-                     });
-                } 
+                    if(currentPresId!=null && currentPresId!=undefined)
+                    {
+                        fs.readFile(CONFIG.presentationDirectory+"/"+currentPresId+".pres.json",function(err,data)
+                        {
+                            if(!!err)
+                            {	
+                                console.error(err);
+                                return;
+                            }
+                            var parsed = JSON.parse(data);
+                            if(parsed.slidArray.length>=1){
+                                switch(json.CMD){
+                                    case "START":
+                                    cptSlide=0;
+                                    break;
+                                    case "BEGIN":
+                                    cptSlide=0;
+                                    break;
+                                    case "END":
+                                    cptSlide=parsed.slidArray.length-1;
+                                    break;
+                                    case "NEXT":
+                                    if(cptSlide<parsed.slidArray.length-1)
+                                    cptSlide=cptSlide+1;
+                                    break;
+                                    case "PREV":
+                                    if(cptSlide>0)
+                                    cptSlide=cptSlide-1;
+                                    break;
+                                }
+                                var currentSlideId= parsed.slidArray[cptSlide].id;
+                                ContentModel.read(currentSlideId, function (err, content) {
+                                    if(!!err)
+                                        {
+                                            console.error(err);
+                                            return err;
+                                        }
+                                content.src = "/contents/" + content.id;
+                                for (var i in socketMap){
+                                    socketMap[i].emit('currentSlidEvent',content);
+                                }
+                                });
+                            }
+                                       
+                       
+                         });
+                    
+                    } 
+                }
             }         
         });
     });
